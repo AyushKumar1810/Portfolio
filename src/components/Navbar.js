2bc4fe0c75c3bbab5d716d7182c8451700e4a09d
Ayush <ayushsri1810@gmail.com>
2024-06-07 15:12:36 +0530

    final commit fork Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Ayushsri1810/Portfolio"
+                href="https://github.com/AyushKumar1810/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
